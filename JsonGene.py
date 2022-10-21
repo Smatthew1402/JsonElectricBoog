770799d8af0a4fffcda802f98daf8d20681f4346
Smatthew1402 <100808047+Smatthew1402@users.noreply.github.com>
2022-10-21 13:32:46 -0400

    Import from csv file created
    Imports from the csv file and creates a list of employees, which contain their information

--- JsonGene.py
+++ JsonGene.py
@@ -1,10 +1,38 @@
 import json as js
+import csv
 from employees import Employee
 
-class JsonGenerator:
-    def __init__(self):
-        pass
 
-    def loademployees(self):
+class JsonGenerator:
+    def __init__(self, filename = None):
+        self.FileName = filename
+
+    def processcsv(self, filename):
+        with open(filename, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            employees = []
+            header = True
+            for row in reader:
+                if header is False:
+                    employees.append(self.loademployees(row))
+                else:
+                    header = False
         #Load data from the csv into a list of employees
-        pass
+                
+    def loademployees(self, list):
+        N = list[0]
+        T = list[1]
+        E = list[2]
+        P = list[3]
+        O = list[4]
+        B = list[5]
+        C = list[6]
+        return Employee(N,T,E,P,O,B,C)
+
+
+        
+
+
+if __name__ == "__main__":
+    t = JsonGenerator()
+    t.processcsv("cmeDeptInfo.csv")
--- employees.py
+++ employees.py
@@ -1,6 +1,6 @@
 class Employee:
     #Name,Title,Email,Phone,Office,Bio,Contact For
-    def __init__(self, Name=None, Title=None, Email=None, Phone=None, Office=None, Bio=None, Contact4=None):
+    def __init__(self, Name='', Title='', Email='', Phone='', Office='', Bio='', Contact4=''):
         self.Name = Name
         self.Title = Title
         self.Email = Email
@@ -9,3 +9,6 @@ class Employee:
         self.Bio = Bio
         self.ContactFor = Contact4
     
+    def __repr__(self):
+        c = ' | '
+        return(str((self.Name, c, self.Title, c, self.Email, c, self.Phone, c, self.Office, c, self.Bio, c, self.ContactFor)))
