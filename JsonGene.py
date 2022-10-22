f8f44987e11d19b6399ec3d8ab59e8089e06f7e5
Smatthew1402 <100808047+Smatthew1402@users.noreply.github.com>
2022-10-21 23:24:34 -0400

    Updated Format and bug fix
    Updated to have an object with the professor's name related to that professor's information.
    
    Fixed an encoding error

--- JsonGene.py
+++ JsonGene.py
@@ -1,7 +1,6 @@
 import json as js
 import csv
 
-
 class JsonGenerator:
     """Generates a Json File from the given csv file. 
         Requires the csv file to be named and formatted specifically
@@ -29,30 +28,36 @@ class JsonGenerator:
                 else:
                     header = False
                 
-    def loademployees(self, list:list) -> dict:
+    def loademployees(self, list:list) -> list:
         """Loads data from the input list into a dict that is returned
 
         Args:
             list (list): the data list that will be cut into a dict
 
         Returns:
-            dict: A dict with the employee's information
+            list: A list containing the employee's name, and a dict of their information
         """
-        employeedict = {"Name": list[0]}
-        employeedict["Title"] = list[1]
-        employeedict["Email"] = list[2]
-        employeedict["Phone"] = list[3]
-        employeedict["Office"] = list[4]
-        employeedict["Bio"] = list[5]
-        employeedict["Contact for"] = list[6]
-        return employeedict
+        employeelist = [str(list[0])]
+        employeedict = {"Title": list[1]}
+        if list[2] != "None Listed":
+            employeedict["Email"] = list[2]
+        if list[3] != "None Listed":
+            employeedict["Phone"] = list[3]
+        if list[4] != "None Listed":
+            employeedict["Office"] = list[4]
+        if list[5] != "None Listed":
+            employeedict["Bio"] = list[5]
+        if list[6] != "None Listed":
+            employeedict["Contact for"] = list[6]
+        employeelist.append(employeedict)
+        return employeelist
 
     def writejson(self):
         """Writes the information in the list of dicts to a json file
         """
         jsonfilename = str(self.DeptName + ".json")
         with open(jsonfilename, 'w') as json:
-            js.dump(self.employees, json, indent="    ")
+            js.dump(self.employees, json, indent = 0, ensure_ascii=False)
         
 
         
